a6848aff3dc702b9f9664a85c1a5a2105601d29c
Naeem Ullah <ktknaeem28@gmail.com>
2026-02-12 09:25:58 +0500

    ..

--- js/main.js
+++ js/main.js
@@ -156,13 +156,9 @@ function initAOS() {
         easing: 'ease-out-back',
         once: true,
         mirror: false,
-        offset: 120, // Card thoda screen ke andar aaye tab animation shuru ho
-        disable: false // Ensure karein ke ye 'false' ho
-    });
-    
-    // Refresh AOS after all images load (Bohat zaroori hai)
-    window.addEventListener('load', function() {
-        AOS.refresh();
+        offset: 50,          // Mobile pe thora jaldi trigger ho jaye
+        disable: false,      // Isay 'false' rakhen taake mobile pe chale
+        disableMutationObserver: false
     });
 }
 
